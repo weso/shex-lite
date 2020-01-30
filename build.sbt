b30f827823a2322c7db979aa7f2b912461c2acbf
Willy <colunga91@gmail.com>
2020-01-30 13:40:06 +0100

    grammar initialized

--- build.sbt
+++ build.sbt
@@ -3,3 +3,5 @@ name := "shExLite"
 version := "0.1"
 
 scalaVersion := "2.12.7"
+
+libraryDependencies += "org.antlr" % "antlr4" % "4.8-1"
---
+++ src/parser/LexerHelper.java
@@ -0,0 +1,62 @@
+package parser;
+
+/**
+ * The Class LexerHelper.
+ *
+ * @author Guillermo Facundo Colunga
+ * @version 201806081225
+ */
+public class LexerHelper {
+
+    /**
+     * From a given string returns the integer that it represents.
+     *
+     * @param str to parse.
+     * @return the integer represented by the string.
+     */
+    public static int lexemeToInt( String str ) {
+        try {
+            return Integer.parseInt( str );
+        } catch (NumberFormatException e) {
+            System.out.println( e );
+        }
+        return -1;
+    }
+
+    /**
+     * From a given string returns the char that matches the given string.
+     *
+     * @param str to parse.
+     * @return the character represented by the string.
+     */
+    public static char lexemeToChar( String str ) {
+        if (str.contains( "\\" )) {
+            if (str.contains( "n" )) {
+                return '\n';
+            } else if (str.contains( "t" )) {
+                return '\t';
+            }
+            str = str.replaceAll( "\\D+", "" );
+            return (char) Integer.parseInt( str );
+
+        } else {
+            return str.charAt( 1 );
+        }
+    }
+
+    /**
+     * From a given string returns the double that matches the given string.
+     *
+     * @param srt to parse.
+     * @return the double represented by the string.
+     */
+    public static double lexemeToReal( String srt ) {
+        try {
+            return Double.parseDouble( srt );
+        } catch (NumberFormatException e) {
+            System.out.println( e );
+        }
+        return -1;
+    }
+
+}
