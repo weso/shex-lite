6b6473d2f80cc7ad96113c19e3e890e3d589316b
thewilly <colunga91@gmail.com>
2020-02-07 16:56:43 +0100

    [ast] further work

---
+++ gen/es/weso/shexl/parser/ShExLiteBaseListener.java
@@ -0,0 +1,175 @@
+// Generated from C:/Users/Usuario/Desktop/github/weso/shex-lite/src/main/java/es.weso.shexl/parser\ShExLite.g4 by ANTLR 4.8
+package es.weso.shexl.parser;
+
+  import es.weso.shexl.*;
+  import java.util.*;
+
+
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+/**
+ * This class provides an empty implementation of {@link ShExLiteListener},
+ * which can be extended to create a listener which only needs to handle a subset
+ * of the available methods.
+ */
+public class ShExLiteBaseListener implements ShExLiteListener {
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterShex_lite_doc(ShExLiteParser.Shex_lite_docContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitShex_lite_doc(ShExLiteParser.Shex_lite_docContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterPrefix_def(ShExLiteParser.Prefix_defContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitPrefix_def(ShExLiteParser.Prefix_defContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterShape_def(ShExLiteParser.Shape_defContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitShape_def(ShExLiteParser.Shape_defContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterShape_body(ShExLiteParser.Shape_bodyContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitShape_body(ShExLiteParser.Shape_bodyContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterConstraint_def(ShExLiteParser.Constraint_defContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitConstraint_def(ShExLiteParser.Constraint_defContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterContraint_id(ShExLiteParser.Contraint_idContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitContraint_id(ShExLiteParser.Contraint_idContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterConstraint_type(ShExLiteParser.Constraint_typeContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitConstraint_type(ShExLiteParser.Constraint_typeContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterProperty_def(ShExLiteParser.Property_defContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitProperty_def(ShExLiteParser.Property_defContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterShape_inv(ShExLiteParser.Shape_invContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitShape_inv(ShExLiteParser.Shape_invContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterPrefix_inv(ShExLiteParser.Prefix_invContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitPrefix_inv(ShExLiteParser.Prefix_invContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterPrimitive_type(ShExLiteParser.Primitive_typeContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitPrimitive_type(ShExLiteParser.Primitive_typeContext ctx) { }
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitTerminal(TerminalNode node) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitErrorNode(ErrorNode node) { }
+}
--- src/main/java/es.weso.shexl/parser/ShExLite.g4
+++ src/main/java/es.weso.shexl/parser/ShExLite.g4
@@ -1,30 +1,36 @@
 grammar ShExLite;
 
-shex_lite_doc
- : prefix_def+ shape_def+ EOF
+@header {
+  import es.weso.shexl.*;
+  import java.util.*;
+}
+
+shex_lite_doc returns [ShExL ast]
+ : {List<Definition> defs = new ArrayList<Definition>();} (prefix_def {defs.add($prefix_def.ast);})+ (shape_def {defs.add($shape_def.ast);})+ EOF {$ast = new ShExL(0,0,defs);}
  ;
 
 // ------------------
 // DEFINITIONS
 // ------------------
 
-prefix_def
- : 'PREFIX' ID? ':' IRI
+prefix_def returns [Definition ast]
+ : 'PREFIX' ID? ':' IRI {$ast = new PrefixDef($IRI.line, $IRI.pos+1, $ID.text, new URI($IRI.line, $IRI.pos+1, $IRI.text));}
  ;
 
-shape_def
- : ':' ID '{' shape_body '}'
+shape_def returns [Definition ast]
+ : ':' ID '{' shape_body '}' {$ast = new ShapeDef($ID.line, $ID.pos+1, $ID.text, $shape_body.ast);}
  ;
 
-shape_body
- : (constraint_def | (constraint_def';')+ constraint_def)
+shape_body returns [List<Constraint> ast = new ArrayList<Constraint>()]
+ : (c1=constraint_def {$ast.add($c1.ast)} | (c2=constraint_def';' {$ast.add($c2.ast)})+ c3=constraint_def {$ast.add($c3.ast)})
  ;
 
-constraint_def
- : contraint_id constraint_type
+constraint_def returns [Constraint $ast]
+ : contraint_id constraint_type {$ast = new ShapeConstraintStmt($contraint_id.line, $contraint_id.pos+1, $contraint_id.text, $contraint_type, null);}
  ;
 
-contraint_id
+contraint_id returns [
+]
  : prefix_inv ':' property_def
  ;
 
