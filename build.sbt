73ed5c694cd00817311c53a5116c07b8f06b1fac
thewilly <colunga91@gmail.com>
2020-02-12 08:15:26 +0100

    [type-checking] adds definition inference

--- build.sbt
+++ build.sbt
@@ -4,4 +4,7 @@ version := "0.1"
 
 scalaVersion := "2.12.7"
 
+Compile / scalaSource := baseDirectory.value / "src/main/scala-2.13"
+Compile / javaSource := baseDirectory.value / "src/main/java"
+
 libraryDependencies += "org.antlr" % "antlr4" % "4.8-1"
--- src/main/scala-2.13/es/weso/shexl/visitor/InvocationsVisitor.scala
+++ src/main/scala-2.13/es/weso/shexl/visitor/InvocationsVisitor.scala
@@ -7,19 +7,23 @@ import es.weso.shexl.symboltable.SymbolTable
 case class InvocationsVisitor() extends AbstractVisitor {
 
   override def visit(shapeInv: ShapeInv, param: Any): Unit = {
-    if( SymbolTable.getShape(shapeInv.shapeName).isEmpty ) {
+    val definition = SymbolTable.getShape(shapeInv.shapeName)
+    if( definition.isEmpty ) {
       val shapeName = shapeInv.shapeName
-      ErrorHandler.addError(new Error(shapeInv.line, shapeInv.column, s"Shape [$shapeName] not defined."))
+      ErrorHandler.addError(Error(shapeInv.line, shapeInv.column, s"Shape [$shapeName] not defined."))
     }
-    //shapeInv.definition.accept(this, param)
+
+    shapeInv.definition = definition.get
   }
 
   override def visit(prefixInv: PrefixInv, param: Any): Unit = {
-    if( SymbolTable.getPrefix(prefixInv.prefixName).isEmpty ) {
+    val definition = SymbolTable.getPrefix(prefixInv.prefixName)
+    if( definition.isEmpty ) {
       val prefixName = prefixInv.prefixName
-      ErrorHandler.addError(new Error(prefixInv.line, prefixInv.column, s"Prefix [$prefixName] not defined."))
+      ErrorHandler.addError(Error(prefixInv.line, prefixInv.column, s"Prefix [$prefixName] not defined."))
     }
-    //prefixInv.definition.accept(this, param)
+
+    prefixInv.definition = definition.get
   }
 
 }
