0f883a08c170d1bc44459fdffcba058697bb6e44
Guillermo Facundo colunga <colunga91@gmail.com>
2020-02-17 12:47:08 +0100

    [repo] add copyright to all files
    Adds the copyright header to all scala/java files

--- src/main/scala-2.13/es/weso/shexl/error/ErrorHandler.scala
+++ src/main/scala-2.13/es/weso/shexl/error/ErrorHandler.scala
@@ -1,21 +1,64 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 WESO Research Group, University of Oviedo.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
 package es.weso.shexl.error
 
 import es.weso.shexl.ast.Error
 
 import scala.collection.mutable.ListBuffer
 
+/**
+ * Error handler system. This system helps to hold the errors found at compile time.
+ */
 object ErrorHandler {
 
+  // List of errors.
   final val errors = new ListBuffer[Error]()
 
+  /**
+   * Returns whether the error handler has errors or not.
+   *
+   * @return true if has errors, false otherwise.
+   */
   def hasErrors: Boolean = {
     !this.errors.isEmpty
   }
 
+  /**
+   * Adds errors to the error system.
+   *
+   * @param error to add to the system.
+   * @return
+   */
   def addError(error: Error) = {
     this.errors += error
   }
 
+  /**
+   * Shows the errors through the terminal.
+   */
   def showErrors(): Unit = {
     for(error <- errors) {
       val line = error.line
--- src/main/scala-2.13/es/weso/shexl/symboltable/SymbolTable.scala
+++ src/main/scala-2.13/es/weso/shexl/symboltable/SymbolTable.scala
@@ -1,3 +1,27 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 WESO Research Group, University of Oviedo.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
 package es.weso.shexl.symboltable
 
 import java.util.Objects
@@ -6,14 +30,18 @@ import es.weso.shexl.ast.{PrefixDef, ShapeDef, ShapeInv}
 
 import scala.collection.mutable.HashMap
 
+/**
+ * Table to store different symbols of the parsed expressions, that is shapes definitions and prefixes.
+ */
 object SymbolTable {
 
   final val prefixes = HashMap[String, PrefixDef]()
   final val shapes = HashMap[String, ShapeDef]()
 
   /**
+   * Inserts a prefix in the table.
    *
-   * @param prefixDef
+   * @param prefixDef is the prefix definition to insert in the table.
    * @return
    */
   def insert(prefixDef: PrefixDef) = {
