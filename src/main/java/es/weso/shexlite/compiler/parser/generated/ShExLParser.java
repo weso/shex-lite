218fba7708bdbe02e9dd10708e62a5002fd234aa
Guillermo Facundo colunga <colunga91@gmail.com>
2020-02-17 18:22:14 +0100

    [legal] add copyright

--- src/main/java/es/weso/shexlite/compiler/parser/generated/ShExLParser.java
+++ src/main/java/es/weso/shexlite/compiler/parser/generated/ShExLParser.java
@@ -1,17 +1,40 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 WESO Research Group, University of Oviedo.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
 // Generated from C:/Users/Willy/Documents/github/weso/shex-lite/src/main/java/es/weso/shexlite/parser\ShExL.g4 by ANTLR 4.8
-package es.weso.shexlite.parser.generated;
+package es.weso.shexlite.compiler.parser.generated;
 
 import es.weso.shexlite.compiler.ast.*;
-import java.util.*;
+
 import java.lang.String;
 
 import org.antlr.v4.runtime.atn.*;
 import org.antlr.v4.runtime.dfa.DFA;
 import org.antlr.v4.runtime.*;
-import org.antlr.v4.runtime.misc.*;
 import org.antlr.v4.runtime.tree.*;
 import java.util.List;
-import java.util.Iterator;
 import java.util.ArrayList;
 
 @SuppressWarnings({"all", "warnings", "unchecked", "unused", "cast"})
