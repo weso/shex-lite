4f8090ff35644baa9c8d829d9581b12ee9d45f20
Guillermo Facundo Colunga <colunga91@gmail.com>
2020-03-14 10:18:18 +0100

    [semantic] move default walker to ast file

--- src/compiler/ast/ast.scala
+++ src/compiler/ast/ast.scala
@@ -124,3 +124,38 @@ private[compiler] trait ASTWalker[TP, TR] {
   def walk(constraint: RealLiteral, param: TP): TR
   def walk(constraint: ValueSetConstraint, param: TP): TR
 }
+
+/**
+  * Default implementation for the AST walker. It only propagates the action to all the children of each node.
+  */
+class DeflautASTWalker extends ASTWalker[Any,Any] {
+  override def walk(schema: Schema, param: Any): Any = schema.statements.map(st => st.walk(this, param))
+  override def walk(statement: Statement, param: Any): Any = null
+  override def walk(statement: ImportStatement, param: Any): Any = statement.iri.walk(this, param)
+  override def walk(statement: DeclarationStmt, param: Any): Any = null
+  override def walk(declaration: PrefixDeclaration, param: Any): Any = declaration.iri.walk(this, param)
+  override def walk(declaration: BaseDeclaration, param: Any): Any = declaration.iri.walk(this, param)
+  override def walk(declaration: StartDeclaration, param: Any): Any = declaration.ref.walk(this, param)
+  override def walk(declaration: ShapeDeclaration, param: Any): Any = declaration.constraint.walk(this, param)
+  override def walk(constraint: Constraint, param: Any): Any = null
+  override def walk(constraint: TripleConstraint, param: Any): Any = {
+    constraint.property.walk(this, param)
+    constraint.constraint.walk(this, param)
+    constraint.cardinality.walk(this, param)
+  }
+  override def walk(constraint: Cardinality, param: Any): Any = null
+  override def walk(constraint: NodeConstraint, param: Any): Any = null
+  override def walk(constraint: LiteralNodeConstraint, param: Any): Any = null
+  override def walk(constraint: IRINodeConstraint, param: Any): Any = null
+  override def walk(constraint: AnyTypeNodeConstraint, param: Any): Any = null
+  override def walk(constraint: BNodeNodeConstraint, param: Any): Any = null
+  override def walk(constraint: NonLiteralNodeConstraint, param: Any): Any = null
+  override def walk(constraint: Invocation, param: Any): Any = constraint.decl.walk(this, param)
+  override def walk(constraint: PrefixInvocation, param: Any): Any = constraint.decl.walk(this, param)
+  override def walk(constraint: ShapeInvocation, param: Any): Any = constraint.decl.walk(this, param)
+  override def walk[TL](constraint: Literal[TL], param: Any): Any = null
+  override def walk(constraint: IRILiteral, param: Any): Any = null
+  override def walk(constraint: StringLiteral, param: Any): Any = null
+  override def walk(constraint: RealLiteral, param: Any): Any = null
+  override def walk(constraint: ValueSetConstraint, param: Any): Any = null
+}
