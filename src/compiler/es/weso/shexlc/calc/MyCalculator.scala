a034c8922b9f79c6cbba8bc7019c2353e03f1052
Pablo Menéndez Suárez ◔_◔ <pabloyo97@hotmail.com>
2020-09-16 13:31:18 +0200

    cucumber -> hello world

---
+++ src/compiler/es/weso/shexlc/calc/MyCalculator.scala
@@ -0,0 +1,10 @@
+package es.weso.shexlc.calc
+
+class MyCalculator {
+  def add(first: Int, second: Int): Int = {
+    first + second
+  }
+  def sub(first: Int, second: Int): Int = {
+    first - second
+  }
+}
---
+++ test/unit/es/weso/shexlc/TestRunner.scala
@@ -0,0 +1,13 @@
+package es.weso.shexlc
+
+import io.cucumber.junit.{Cucumber, CucumberOptions}
+import org.junit.runner.RunWith
+
+@RunWith(classOf[Cucumber])
+@CucumberOptions(
+  features = Array("test/features"),
+  glue = Array("es.weso.shexlc"),
+  plugin = Array("pretty",
+    "html:target/cucumber")
+)
+class TestRunner {}
